7eb33bb99aebe353e7e022a4de46f254f66e8fa9
Aidan Breen <aido179@yahoo.com>
2017-09-08 10:06:05 +0100

    update paralax

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ function callbackFunc() {
     $(".nav_container_default").css({
       'top':"-"+((y/1.5)+1)+"px"
     });
+
     $("#header_top").css({
-      'margin-top':((y/20)-3)+"em"
+      'margin-top':((y/100)-3)+"em"
     });
     $("#header").css({
       'background':`linear-gradient( to right, rgba(0, 0, 0, ${y/400}), rgba(45, 29, 39, ${y/400}), rgba(46, 47, 70, ${y/400}), rgba(0, 0, 0, ${y/400}) ), url(city2.jpeg) no-repeat center center`,
