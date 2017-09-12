783a29b1cd079efd4d2b37d44afab725e51221a8
Aidan Breen <aido179@yahoo.com>
2017-09-12 13:49:53 +0100

    Add progressive image loading and optimized images

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+var bgimg = "city2_small.jpeg";
+
+$(document).ready(function(){
+  $("#full-res-image-loader").on('load',function(){
+    bgimg = "city2.jpeg";
+    $("#header").css({
+      'background': `url(city2.jpeg) no-repeat center center`,
+      'background-size': 'cover'
+    });
+  }).each(function() {
+    if(this.complete) $(this).trigger('load');
+  });
+});
+
+
+
 window.addEventListener("scroll", callbackFunc);
 function callbackFunc() {
   var y = window.pageYOffset;
   if (y > 1) {
     //scrolled header
-    //$(".nav_container_default").addClass('nav_container_scroll');
     $(".nav_container_default").css({
       'top':"-"+((y/1.5)+1)+"px"
     });
@@ -12,12 +27,10 @@ function callbackFunc() {
       'margin-top':((y/100)-3)+"em"
     });
     $("#header").css({
-      'background':`linear-gradient( to right, rgba(0, 0, 0, ${y/400}), rgba(45, 29, 39, ${y/400}), rgba(46, 47, 70, ${y/400}), rgba(0, 0, 0, ${y/400}) ), url(city2.jpeg) no-repeat center center`,
+      'background':`linear-gradient( to right, rgba(0, 0, 0, ${y/400}), rgba(45, 29, 39, ${y/400}), rgba(46, 47, 70, ${y/400}), rgba(0, 0, 0, ${y/400}) ), url(${bgimg}) no-repeat center center`,
       'background-size': 'cover'
     });
   } else {
     //default header
-    //$(".nav_container_default").removeClass('nav_container_scroll');
-
   }
 }
